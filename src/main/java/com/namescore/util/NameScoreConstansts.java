b4714f258ec63bcc0648c25e6fd4b69374881939
Viswa2216 <k.kashiviswanath@gmail.com>
2021-03-28 20:36:34 -0400

    modified constants file

--- src/main/java/com/namescore/util/NameScoreConstansts.java
+++ src/main/java/com/namescore/util/NameScoreConstansts.java
@@ -12,6 +12,6 @@ public class NameScoreConstansts {
 	public static final String NAME_SCORE ="NAMESCORE";
 	public static final String EMPLOYEE_NAME_SCORE ="EMPLOYEENAMESCORE";
 	public static final String FIRST_LAST_NAME_SCORE ="FIRSTLASTNAMESCORE";
-	public static final String ALPHA_REGEX = "^[a-zA-Z]*$";
+	public static final String ALPHA_REGEX = "^[A-Z]*$";
 	public static final String COMMA_DELIMETER=",";
 }
